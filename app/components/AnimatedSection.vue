5c2fe56a80ffe770dac8ef4a753471eab911d2e7
donfreddy <freddytamwo@gmail.com>
2026-06-10 16:01:19 +0100

    remove last motion-v usage from AnimatedSection.vue

--- app/components/AnimatedSection.vue
+++ app/components/AnimatedSection.vue
@@ -1,8 +1,5 @@
 <template>
-  <Motion
-    :initial="{ opacity: 0, y: 40 }"
-    :visible="{ opacity: 1, y: 0, transition: { duration: 0.7, ease: 'easeOut' } }"
-  >
+  <div>
     <slot />
-  </Motion>
+  </div>
 </template>
